7e1a9bb62080162a2d76ede55dc11a3b6c97322a
shaishabcoding <myshaishab@gmail.com>
2024-05-12 08:12:15 +0600

    added borrow func

--- index.js
+++ index.js
@@ -2,13 +2,37 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const moment = require("moment");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+//jwt middleware
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = { email: decoded };
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bhmvsbs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -32,6 +56,7 @@ async function run() {
 
     const booksDB = client.db("bookDB");
     const booksCollection = booksDB.collection("books");
+    const borrowsCollection = booksDB.collection("borrows");
 
     app.get("/books", async (req, res) => {
       const result = await booksCollection.find().toArray();
@@ -51,15 +76,62 @@ async function run() {
       res.send(result);
     });
 
-    app.put("/book/:id/borrow", async (req, res) => {
+    app.put("/book/:id/borrow", verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
 
-      const result = await booksCollection.updateOne(query, {
-        $inc: { quantity: -1 },
+      const { email } = req.user;
+      const { returnDate, name } = req.body;
+
+      const userBorrowedBooksCount = await borrowsCollection.countDocuments({
+        borrowerEmail: email,
       });
 
-      res.send(result);
+      // If the user has borrowed less than 3 books, allow borrowing
+      if (userBorrowedBooksCount < 3) {
+        try {
+          // Begin a session
+          const session = client.startSession();
+          session.startTransaction();
+
+          // Update the book quantity
+          const result = await booksCollection.updateOne(query, {
+            $inc: { quantity: -1 },
+          });
+
+          // Create a borrowing record
+          const borrowRecord = {
+            bookId: id,
+            borrowerEmail: email,
+            borrowDate: moment().format("MM/DD/YYYY"),
+            returnDate,
+            name,
+          };
+
+          console.log(borrowRecord);
+
+          // Insert the borrowing record
+          await borrowsCollection.insertOne(borrowRecord, { session });
+
+          // Commit the transaction
+          await session.commitTransaction();
+          session.endSession();
+
+          res.send(result);
+        } catch (error) {
+          console.log(error);
+          // If an error occurs, abort the transaction
+          await session.abortTransaction();
+          session.endSession();
+
+          console.error(error);
+          res.status(500).send("Error borrowing book.");
+        }
+      } else {
+        res
+          .status(403)
+          .send("You have reached the maximum limit of 3 borrowed books.");
+      }
     });
   } finally {
     // await client.close();
@@ -70,6 +142,33 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("server running");
 });
+
+//jwt routes -- start
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
+//creating Token
+app.post("/jwt", async (req, res) => {
+  const { email } = req.body;
+  console.log("user for token", email);
+  const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
+  console.log(token);
+  res.cookie("token", token, cookieOptions).send({ success: true });
+});
+
+//clearing Token
+app.post("/logout", async (req, res) => {
+  const { email } = req.body;
+  console.log("logging out", email);
+  res
+    .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+    .send({ success: true });
+});
+
+//jwt routes -- end
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
